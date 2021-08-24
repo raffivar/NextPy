bb9e0d12e1f3675ee6b64e973ed91e3b7b82e818
cookienator <rafivar@gmail.com>
2021-08-24 18:13:22 +0300

    Implemented parse_ranges exercise

---
+++ unit_4/parse_ranges.py
@@ -0,0 +1,20 @@
+def parse_ranges(ranges):
+    ranges_str_gen = (rng for rng in ranges.split(','))
+    ranges_tup_gen = ((rng.split('-')) for rng in ranges_str_gen)
+    ranges_rng_gen = (range(int(rng[0]), int(rng[1]) + 1) for rng in ranges_tup_gen)
+    rng_gen = (rng for rng in ranges_rng_gen)
+    num_gen = (num for rng in rng_gen for num in rng)
+
+    lst = []
+    for num in num_gen:
+        lst.append(num)
+    return lst
+
+
+def main():
+    print(list(parse_ranges("1-2,4-4,8-10")))
+    print(list(parse_ranges("0-0,4-8,20-21,43-45")))
+
+
+if __name__ == "__main__":
+    main()
