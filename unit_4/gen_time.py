aa1ca2a83cc5d316fc34d46e1b04ad0f2659e18d
cookienator <rafivar@gmail.com>
2021-08-25 14:45:49 +0300

    improved (partially) gen_time exercise according to requirements

--- unit_4/gen_time.py
+++ unit_4/gen_time.py
@@ -37,15 +37,16 @@ def gen_months():
 
 def gen_days(month, leap_year=True):
     stop = 0
-    if month in (1, 3, 5, 7, 8, 10, 12):
+    if month == 2:
+        if leap_year:
+            stop = 30
+        else:
+            stop = 29
+    elif month in (1, 3, 5, 7, 8, 10, 12):
         stop = 32
-    elif month in (2, 4, 6, 9, 11):
+    elif month in (4, 6, 9, 11):
         stop = 31
-        if month == 2:
-            if leap_year:
-                stop = 30
-            else:
-                stop = 29
+
     for i in range(1, stop):
         yield i
 
@@ -59,10 +60,14 @@ def gen_date():
 
 def main():
     gd = gen_date()
+
     for i in range(5):
         date = next(gd)
         for gt in gen_time():
             print("{} {}".format(date, gt))
+
+    # for i in range(700):
+    #     print(next(gd))
 
 
 if __name__ == "__main__":
