242464fa8391ab6ac6d46fb5021f6407188ffbb6
cookienator <rafivar@gmail.com>
2021-08-26 14:12:52 +0300

    implemented bills exercise

---
+++ unit_5/dollars.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+
+def main():
+    bills = [20, 20, 20, 10, 10, 10, 10, 10, 5, 5, 1, 1, 1, 1, 1]
+    bill_amt = 1
+    while bill_amt <= len(bills):
+        combos = combinations(bills, bill_amt)
+        combo_set = set(filter(lambda st: sum(st) == 100, combos))
+        if len(combo_set) > 0:
+            print("with {} bills: {}".format(bill_amt, combo_set))
+        bill_amt += 1
+
+
+if __name__ == "__main__":
+    main()
